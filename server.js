2ee2343d84ba06f143c31252c570c2e7939a5b33
Mikhail Malisov <malisov.mikhail@gmail.com>
2022-12-01 18:37:14 +0400

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = new Server().listen(server)
-server.listen(3001)
+server.listen(process.env.PORT || 3001)
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/app/index.html')
 })
